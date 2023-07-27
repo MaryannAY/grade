2858c46facc17542336a13e8b6b608bc07eaca95
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-27 02:39:07 -0700

    Answer to Grade

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const grade=90
+
+if (grade<100 || grade>90){
+    console.log("you got an A")
+}
+if (grade<90 || grade>75){
+    console.log("you got a B")
+}
+if (grade<75 || grade>60){
+    console.log("you got a C")
+} if (grade>60){
+    console.log("you failed")
+}
